dd700b2f2374c724ac2190b96849e0b9fc129877
nyun-nye <2001_11_7@naver.com>
2026-07-31 12:00:44 +0900

    ✨: MatePage 및 CapsulePage 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import TestPage from './pages/TestPage'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import CapsulePage from "./pages/CapsulePage";
+import HomePage from "./pages/HomePage";
+import MatePage from "./pages/MatePage";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/mate" element={<MatePage />} />
+        <Route path="/mate/capsule" element={<CapsulePage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
