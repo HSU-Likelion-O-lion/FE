5b4facb3d09fe2ef5c685a08a5303fbaa5758615
nyun-nye <2001_11_7@naver.com>
2026-08-03 10:43:32 +0900

    ✨: 프로필 관련 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import CapsulePage from "./pages/CapsulePage";
 import HomePage from "./pages/HomePage";
 import MatePage from "./pages/MatePage";
+import BackupSyncPage from "./pages/profile/BackupSyncPage";
+import InquiryPage from "./pages/profile/InquiryPage";
+import NoticePage from "./pages/profile/NoticePage";
+import PrivacyPage from "./pages/profile/PrivacyPage";
+import ProfileEditPage from "./pages/profile/ProfileEditPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import PushNotificationPage from "./pages/profile/PushNotificationPage";
+import TermsPage from "./pages/profile/TermsPage";
 import TestPage from "./pages/TestPage";
 
 function App() {
@@ -11,6 +19,14 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/mate" element={<MatePage />} />
         <Route path="/mate/capsule" element={<CapsulePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile/edit" element={<ProfileEditPage />} />
+        <Route path="/profile/push" element={<PushNotificationPage />} />
+        <Route path="/profile/backup" element={<BackupSyncPage />} />
+        <Route path="/profile/notice" element={<NoticePage />} />
+        <Route path="/profile/inquiry" element={<InquiryPage />} />
+        <Route path="/profile/terms" element={<TermsPage />} />
+        <Route path="/profile/privacy" element={<PrivacyPage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
